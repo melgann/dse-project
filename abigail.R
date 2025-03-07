06108094334e545b88a9c8b4b8c83bf7f62874bd
Abigail <abigail.tan.he@gmail.com>
2025-03-07 15:30:18 +0800

    changes file reading path

--- abigail.R
+++ abigail.R
@@ -3,10 +3,10 @@ library(tidyverse)
 library(stringr)
 
 # cleaning rental dataset
-q1_rental_og = read.csv("CommercialRentalStatsByStreet20250303231245.csv")
-q2_rental_og = read.csv("CommercialRentalStatsByStreet20250303230811.csv")
-q3_rental_og = read.csv("CommercialRentalStatsByStreet20250303230853.csv")
-q4_rental_og = read.csv("CommercialRentalStatsByStreet20250303231354.csv")
+q1_rental_og = read.csv("Raw_datasets/CommercialRentalStatsByStreet20250303231245.csv")
+q2_rental_og = read.csv("Raw_datasets/CommercialRentalStatsByStreet20250303230811.csv")
+q3_rental_og = read.csv("Raw_datasets/CommercialRentalStatsByStreet20250303230853.csv")
+q4_rental_og = read.csv("Raw_datasets/CommercialRentalStatsByStreet20250303231354.csv")
 # prices are per square meter per month
 
 rental_prices = rbind(q1_rental_og,q2_rental_og,q3_rental_og,q4_rental_og)%>%
@@ -21,7 +21,5 @@ rental_prices = rental_prices  %>%
   summarise(avg_median_price=mean(price_median, na.rm=TRUE))
 
 #write csv
-write.csv(rental_prices, "rental_prices.csv", row.names = FALSE)
+#write.csv(rental_prices, "rental_prices.csv", row.names = FALSE)
   
-# read in planning area
-street_name_planning_area = read.csv("street_name_planning_area.csv")
